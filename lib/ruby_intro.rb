4da5598bc75d01e7fd9c711c05a8c81b648a4122
enablex3 <ereyes@citadel.edu>
2018-09-06 22:47:18 +0000

    Q3 part 2 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -91,13 +91,16 @@ def binary_multiple_of_4?(s)
   # Convert string to integer via 2's compliment
   @str = s
   @num = @str.to_i(2)
+
+  # "0" is still valid since zero is divisible by anything
+  # The 0 as the result of an invalid string means the input string was invalid!
   while @str != "0"
         if @num == 0
                 return false
         end
         break
   end
-
+  # Check for numbers divisible by 4
   if @num % 4 == 0
         return true
   else
@@ -108,5 +111,24 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  # Intialize attributes
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  # Set the constructor
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price.to_f
+    # Make sure isbn is type String
+    if @isbn.is_a? String
+            # Raise error if isbn is empty or price <= 0
+            if @isbn.empty? || @price <= 0
+                    raise ArgumentError
+            end
+    end
+  end
+  
+  # This method will return the price as a string
+  def price_as_string(price)
+  end
 end
